2e2922f67ccdbec9db6f9739dfaa5c27434b3a47
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-05-01 03:40:39 +0000

    add url to cors

--- setup/settings.py
+++ setup/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "https://zzr90rrg-8000.brs.devtunnels.ms",
     "https://zzr90rrg-3000.brs.devtunnels.ms",
     "https://sturdy-umbrella-4vvwj6rgrxhq4pp-8000.app.github.dev"
